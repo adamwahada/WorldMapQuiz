9ac895fee0a08515b68725f5827d3ff4f7f30d15
Adam Wahada <adam.wahada@esprit.tn>
2025-12-11 05:41:32 +0100

    fixed ve

--- src/environments/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB4909j3HUfU_C0qtbVBLKoWrfxKq1YLXk",
-  authDomain: "world-quizet.firebaseapp.com",
-  projectId: "world-quizet",
-  storageBucket: "world-quizet.firebasestorage.app",
-  messagingSenderId: "404708679350",
-  appId: "1:404708679350:web:960f476d712cfc384a1017",
-  measurementId: "G-DNYZ8FYC7K"
-};
